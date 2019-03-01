3c0bacb094a1cae8f5dc44a74a3b4c4e6543806f
henrikschytze <henrikschytze@gmail.com>
2019-03-01 12:16:27 +0100

    development changes

--- demo/demo.js
+++ demo/demo.js
@@ -1,4 +1,10 @@
 import {
     spriteScroller
-} from "../src/js/spriteScroller.js";
-console.log(spriteScroller);
+} from "../dist/spriteScroller.js";
+let spriteOne = new spriteScroller({
+    element: "sprite",
+    rows: 46,
+    columns: 1,
+    direction: "vertical"
+});
+spriteOne.init();
--- src/js/spriteScroller.js
+++ src/js/spriteScroller.js
@@ -1,19 +1,65 @@
-export const spriteScroller = class SpriteScroller {
+// Import dependencies
+import ScrollMagic from "scrollmagic";
+import "scrollmagic/scrollmagic/uncompressed/plugins/animation.gsap";
+import StickyFill from "stickyfill";
+import TweenMax, {
+    SteppedEase,
+} from "gsap";
+
+export const spriteScroller = class spriteScroller {
     constructor({
         element,
-        frames,
-        duration = 600,
-        columns,
-        rows,
-        startingPosition = null,
-        iterations = Infinity
+        columns = 1,
+        rows = 1,
+        direction = "horizontal"
     }) {
         this.element = element;
-        this.frames = frames;
-        this.duration = duration / frames;
         this.columns = columns;
         this.rows = rows;
-        this.startingPosition = startingPosition;
-        this.iterations = iterations;
+        this.direction = direction;
+        this.init = function () {
+            let {
+                element,
+                columns,
+                rows,
+                direction
+            } = this;
+            // Declare background-position values for when creating tween later
+            let backgroundPositionValues;
+            // Initialize stickyfill instance
+            let stickyfill = StickyFill();
+            // Get the element
+            let spriteElement = document.querySelector(`.${element}`);
+            // Set background size of sprite:
+            spriteElement.style.backgroundSize = `${columns * 100}% ${rows * 100}%`;
+            let spriteContainerElement = document.querySelector(".sitcky-wrapper");
+            // Use stickyfill for IE support
+            stickyfill.add(spriteContainerElement);
+            // Initialize the scrollmagic controller
+            let controller = new ScrollMagic.Controller();
+            // Create the tween
+            // The background-position depends on the direction
+            switch (direction) {
+                case "vertical":
+                    backgroundPositionValues = "0% 100%"
+                    break;
+                case "horizontal":
+                    backgroundPositionValues = "100% 0%"
+                    break;
+            };
+
+            let tween = TweenMax.to(spriteElement, 1.0, {
+                backgroundPosition: backgroundPositionValues,
+                ease: SteppedEase.config(rows - 1)
+            });
+            // Build scene
+            let scene = new ScrollMagic.Scene({
+                    triggerHook: "onEnter",
+                    duration: 1000
+                })
+                // animate color and top border in relation to scroll position
+                .setTween(tween) // the tween durtion can be omitted and defaults to 1
+                .addTo(controller)
+        };
     }
 }
